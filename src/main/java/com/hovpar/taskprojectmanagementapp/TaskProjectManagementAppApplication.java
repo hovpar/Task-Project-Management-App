bfba9ac5d8d68907907499b21809a2314751abc5
Hovhannes Partizpanyan <hovpar@icloud.com>
2025-10-01 13:05:14 +0200

    add base structure

---
+++ src/main/java/com/hovpar/taskprojectmanagementapp/TaskProjectManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.hovpar.taskprojectmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskProjectManagementAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(TaskProjectManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hovpar/taskprojectmanagementapp/TaskProjectManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hovpar.taskprojectmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskProjectManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
